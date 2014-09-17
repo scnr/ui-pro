2ff7b2cdf9e1581214676898bb329021592d06ac
Tasos Laskos <tasos.laskos@gmail.com>
2014-09-18 00:36:56 +0300

    GlobalProfilePolicy#permitted_parameters: Cleaned up

--- app/policies/global_profile_policy.rb
+++ app/policies/global_profile_policy.rb
@@ -20,13 +20,22 @@ class GlobalProfilePolicy < ApplicationPolicy
         # * browser_cluster_worker_time_to_live => 100
         # * browser_cluster_ignore_images       => true
         [
-            :scope_auto_redundant_paths, :scope_directory_depth_limit,
-            :scope_exclude_binaries, :scope_include_subdomains,
-            :scope_https_only, :scope_dom_depth_limit, :http_request_concurrency,
-            :http_request_redirect_limit, :http_request_timeout,
-            :http_request_queue_size, :browser_cluster_pool_size,
-            :browser_cluster_job_timeout, :browser_cluster_worker_time_to_live,
-            :browser_cluster_ignore_images, { plugins: {} }, :http_response_max_size
+            { plugins: {} },
+            :scope_auto_redundant_paths,
+            :scope_directory_depth_limit,
+            :scope_exclude_binaries,
+            :scope_include_subdomains,
+            :scope_https_only,
+            :scope_dom_depth_limit,
+            :http_request_concurrency,
+            :http_request_redirect_limit,
+            :http_request_timeout,
+            :http_request_queue_size,
+            :http_response_max_size,
+            :browser_cluster_pool_size,
+            :browser_cluster_job_timeout,
+            :browser_cluster_worker_time_to_live,
+            :browser_cluster_ignore_images
         ]
     end
 end
