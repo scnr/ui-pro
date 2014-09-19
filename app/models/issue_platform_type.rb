8eacc0eb9bc1ccf72a7b9d5a3df006332ba32656
Tasos Laskos <tasos.laskos@gmail.com>
2014-09-20 01:10:24 +0300

    IssuePlatformType: Fixed platforms association

--- app/models/issue_platform_type.rb
+++ app/models/issue_platform_type.rb
@@ -1,3 +1,4 @@
 class IssuePlatformType < ActiveRecord::Base
-    has_many :platforms, class_name: 'IssuePlatform'
+    has_many :platforms, class_name: 'IssuePlatform',
+             foreign_key: 'issue_platform_type_id'
 end
