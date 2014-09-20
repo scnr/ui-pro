dff8610c5502de218bda7e8dc59c61de1ca5d4fc
Tasos Laskos <tasos.laskos@gmail.com>
2014-09-20 04:16:21 +0300

    Added Report

---
+++ db/migrate/20140920011411_create_reports.rb
@@ -0,0 +1,10 @@
+class CreateReports < ActiveRecord::Migration
+    def change
+        create_table :reports do |t|
+            t.belongs_to :revision, index: true
+            t.text :location
+
+            t.timestamps
+        end
+    end
+end
---
+++ spec/factories/reports.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+    factory :report do
+        revision nil
+        location '/tmp/report.afr'
+    end
+end
