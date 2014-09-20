4aa26c70157d2337d0442b64336dcb0203513be6
Tasos Laskos <tasos.laskos@gmail.com>
2014-09-20 12:07:48 +0300

    IssuePageDom: execution_flow_sink => execution_flow_sinks

--- app/models/issue_page_dom.rb
+++ app/models/issue_page_dom.rb
@@ -1,7 +1,7 @@
 class IssuePageDom < ActiveRecord::Base
     belongs_to :page, class_name: 'IssuePage', foreign_key: 'issue_page_id'
 
-    has_many   :transitions,         class_name: 'IssuePageDomTransition'
-    has_many   :data_flow_sinks,     class_name: 'IssuePageDomDataFlowSink'
-    has_many   :execution_flow_sink, class_name: 'IssuePageDomExecutionFlowSink'
+    has_many :transitions,          class_name: 'IssuePageDomTransition'
+    has_many :data_flow_sinks,      class_name: 'IssuePageDomDataFlowSink'
+    has_many :execution_flow_sinks, class_name: 'IssuePageDomExecutionFlowSink'
 end
--- spec/models/issue_page_dom_spec.rb
+++ spec/models/issue_page_dom_spec.rb
@@ -4,5 +4,5 @@ describe IssuePageDom do
     expect_it { to belong_to :page }
     expect_it { to have_many :transitions }
     expect_it { to have_many :data_flow_sinks }
-    expect_it { to have_many :execution_flow_sink }
+    expect_it { to have_many :execution_flow_sinks }
 end
