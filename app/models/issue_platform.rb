8ae9539b244a091878a52339da35297a871c7a09
Tasos Laskos <tasos.laskos@gmail.com>
2014-09-20 03:56:08 +0300

    IssuePlatform: has_many :issues

--- app/models/issue_platform.rb
+++ app/models/issue_platform.rb
@@ -1,4 +1,6 @@
 class IssuePlatform < ActiveRecord::Base
     belongs_to :type, class_name: 'IssuePlatformType',
                foreign_key: 'issue_platform_type_id'
+
+    has_many :issues
 end
--- spec/models/issue_platform_spec.rb
+++ spec/models/issue_platform_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe IssuePlatform do
     expect_it { to belong_to :type }
+    expect_it { to have_many :issues }
 end
