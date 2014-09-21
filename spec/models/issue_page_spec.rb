57097093d9186c5a0f4af70dbd21d063430502d9
Tasos Laskos <tasos.laskos@gmail.com>
2014-09-21 08:46:25 +0300

    IssuePage spec: Added .create_from_arachni

--- spec/models/issue_page_spec.rb
+++ spec/models/issue_page_spec.rb
@@ -6,4 +6,25 @@ describe IssuePage do
     expect_it { to have_one :request }
     expect_it { to have_one :response }
     expect_it { to have_one :dom }
+
+    describe '.create_from_arachni' do
+        let(:arachni_page) do
+            Factory[:page]
+        end
+
+        it "creates a #{described_class} from #{Arachni::Page}" do
+            page = described_class.create_from_arachni( arachni_page ).reload
+            expect(page).to be_valid
+
+            expect(page.request).to be_kind_of HttpRequest
+            expect(page.request).to be_valid
+
+            expect(page.response).to be_kind_of HttpResponse
+            expect(page.response).to be_valid
+
+            expect(page.dom).to be_kind_of IssuePageDom
+            expect(page.dom).to be_valid
+        end
+    end
+
 end
