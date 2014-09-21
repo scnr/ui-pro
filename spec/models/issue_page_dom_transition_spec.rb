1692a6c4aefe69fc88837a7bcb1697a76b04a21c
Tasos Laskos <tasos.laskos@gmail.com>
2014-09-21 08:15:48 +0300

    IssuePageDomTransition spec: Added .create_from_arachni

--- spec/models/issue_page_dom_transition_spec.rb
+++ spec/models/issue_page_dom_transition_spec.rb
@@ -2,4 +2,20 @@ require 'spec_helper'
 
 describe IssuePageDomTransition do
     expect_it { to belong_to :dom }
+
+    describe '.create_from_arachni' do
+        let(:arachni_transition) do
+            Factory[:transition]
+        end
+
+        it "creates a #{described_class} from #{Arachni::Page::DOM::Transition}" do
+            frame = described_class.create_from_arachni( arachni_transition ).reload
+            expect(frame).to be_valid
+
+            expect(frame.element).to eq arachni_transition.element.to_s
+            expect(frame.event).to eq arachni_transition.event.to_s
+            expect(frame.time).to eq arachni_transition.time
+        end
+    end
+
 end
