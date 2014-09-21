bfa42dc11c1021d63bd8a1ae606395fc6c049d48
Tasos Laskos <tasos.laskos@gmail.com>
2014-09-21 10:19:35 +0300

    Issue: Added .create_from_arachni

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,8 +1,9 @@
 class Issue < ActiveRecord::Base
-    belongs_to :page, class_name: 'IssuePage', foreign_key: 'issue_page_id'
+    belongs_to :page, class_name: 'IssuePage', foreign_key: 'issue_page_id',
+               dependent: :destroy
 
     belongs_to :referring_page, class_name: 'IssuePage',
-               foreign_key: 'issue_page_id'
+               foreign_key: 'issue_page_id', dependent: :destroy
 
     belongs_to :type, class_name: 'IssueType', foreign_key: 'issue_type_id'
 
@@ -11,10 +12,18 @@ class Issue < ActiveRecord::Base
 
     belongs_to :revision
 
-    has_one  :vector,  as: :with_vector
-    has_many :remarks, class_name: 'IssueRemark', foreign_key: 'issue_id'
+    has_one  :vector,  as: :with_vector, dependent: :destroy
+    has_many :remarks, class_name: 'IssueRemark', foreign_key: 'issue_id',
+                dependent: :destroy
 
     def self.create_from_arachni( issue )
+        issue_remarks = []
+        issue.remarks.each do |author, remarks|
+            remarks.each do |remark|
+                issue_remarks << IssueRemark.create( author: author, text: remark )
+            end
+        end
+
         create(
             type:           IssueType.find_by_check_shortname( issue.check[:shortname] ),
             digest:         issue.digest.to_s,
@@ -24,7 +33,9 @@ class Issue < ActiveRecord::Base
             active:         issue.active?,
             page:           IssuePage.create_from_arachni( issue.page ),
             referring_page: IssuePage.create_from_arachni( issue.referring_page ),
-            vector:         Vector.create_from_arachni( issue.vector )
+            vector:         Vector.create_from_arachni( issue.vector ),
+            remarks:        issue_remarks,
+            platform:       (IssuePlatform.find_by_shortname( issue.platform_name.to_s ) if issue.platform_name),
         )
     end
 end
--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -8,4 +8,38 @@ describe Issue do
     expect_it { to belong_to :platform }
     expect_it { to have_one :vector }
     expect_it { to have_many :remarks }
+
+    describe '.create_from_arachni' do
+        let(:arachni_issue) do
+            Factory[:issue]
+        end
+
+        it "creates a #{described_class} from #{Arachni::Issue}" do
+            platform = IssuePlatform.create(
+                shortname: arachni_issue.platform_name,
+                name:      arachni_issue.platform_name.to_s.upcase
+            )
+
+            issue = described_class.create_from_arachni( arachni_issue ).reload
+            expect(issue).to be_valid
+
+            expect(issue.page).to be_kind_of IssuePage
+            expect(issue.page).to be_valid
+
+            expect(issue.referring_page).to be_kind_of IssuePage
+            expect(issue.referring_page).to be_valid
+
+            expect(issue.vector).to be_kind_of Vector
+            expect(issue.vector).to be_valid
+
+            expect(issue.platform).to eq platform
+
+            expect(issue.remarks).to be_any
+            issue.remarks.each do |remark|
+                expect(remark).to be_kind_of IssueRemark
+                expect(remark).to be_valid
+            end
+        end
+    end
+
 end
