5262fd0cddc4aa5a5211d301a5b430acdb9113af
Tasos Laskos <tasos.laskos@gmail.com>
2014-09-21 07:22:48 +0300

    IssuePageDomFunction spec: Added .create_from_arachni

--- spec/models/issue_page_dom_function_spec.rb
+++ spec/models/issue_page_dom_function_spec.rb
@@ -11,4 +11,32 @@ describe IssuePageDomFunction do
         end
     end
 
+    describe '.create_from_arachni' do
+        let(:source) do
+            "function decodeURI() {
+                 [native code]
+             }"
+        end
+        let(:arguments) do
+            ["#%7Cinput%7Cdefault%3Csome_dangerous_input_c19b39d05da8ac6c4a1643ad7b2ca89b/%3E"]
+        end
+        let(:name) { 'name' }
+
+        let(:arachni_function) do
+            Arachni::Browser::Javascript::TaintTracer::Frame::CalledFunction.new(
+                name:      name,
+                source:    source,
+                arguments: arguments
+            )
+        end
+
+        it "creates a #{described_class} from #{Arachni::Browser::Javascript::TaintTracer::Frame::CalledFunction}" do
+            function = described_class.create_from_arachni( arachni_function )
+
+            expect(function.name).to eq arachni_function.name
+            expect(function.source).to eq arachni_function.source
+            expect(function.arguments).to eq arachni_function.arguments
+        end
+    end
+
 end
