ce7081e0d57064294be7c057e18be9e374364ba4
Tasos Laskos <tasos.laskos@gmail.com>
2014-09-21 07:11:06 +0300

    HttpResponse spec: Added case for .create_from_arachni

--- spec/models/http_response_spec.rb
+++ spec/models/http_response_spec.rb
@@ -11,4 +11,46 @@ describe HttpResponse do
         end
     end
 
+    describe '.create_from_arachni' do
+        let(:url) { 'http://test.com/stuff' }
+        let(:code) { 200 }
+        let(:time) { 0.012 }
+        let(:ip_address) { '127.0.0.1' }
+        let(:return_code) { 'ok' }
+        let(:return_message) { 'No error' }
+        let(:body) { '<html>stuff' }
+        let(:headers) do
+            {
+                'User-Agent' => 'Arachni/v1.0',
+                'From'       => 'tasos.laskos@test.com'
+            }
+        end
+
+        let(:arachni_response) do
+            Arachni::HTTP::Response.new(
+                url:            url,
+                code:           code,
+                time:           time,
+                ip_address:     ip_address,
+                return_code:    return_code,
+                return_message: return_message,
+                body:           body,
+                headers:        headers
+            )
+        end
+
+        it "creates a #{described_class} from #{Arachni::HTTP::Response}" do
+            response = described_class.create_from_arachni( arachni_response )
+
+            expect(response.url).to eq arachni_response.url
+            expect(response.code).to eq arachni_response.code
+            expect(response.ip_address).to eq arachni_response.ip_address
+            expect(response.return_code).to eq arachni_response.return_code
+            expect(response.return_message).to eq arachni_response.return_message
+            expect(response.body).to eq arachni_response.body
+            expect(response.headers).to eq arachni_response.headers
+            expect(response.raw_headers).to eq arachni_response.headers_string
+        end
+    end
+
 end
