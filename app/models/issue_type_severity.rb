b232b0d199128eaa45480eb11f0ad2cf17ebff2b
Tasos Laskos <tasos.laskos@gmail.com>
2014-09-23 10:30:48 +0300

    IssueTypeSeverity: Tiny cleanup

--- app/models/issue_type_severity.rb
+++ app/models/issue_type_severity.rb
@@ -2,7 +2,9 @@ class IssueTypeSeverity < ActiveRecord::Base
     has_many :types, class_name: 'IssueType'
     has_many :issues, through: :types
 
-    [:high, :medium, :low, :informational].each do |severity|
+    SEVERITIES = [:high, :medium, :low, :informational]
+
+    SEVERITIES.each do |severity|
         define_singleton_method severity do
             where( name: severity.to_s ).first
         end
