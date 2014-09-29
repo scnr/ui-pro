c9bd3bb9148922ca79942c4ad784d74d9e727671
Tasos Laskos <tasos.laskos@gmail.com>
2014-09-29 10:41:49 +0300

    Revision: Set #index on before_save

--- app/models/revision.rb
+++ app/models/revision.rb
@@ -3,9 +3,11 @@ class Revision < ActiveRecord::Base
     has_many :issues,  dependent: :destroy
     has_many :sitemap_entries, counter_cache: true
 
-    after_create :set_index
+    validates_presence_of :scan
+
+    before_save :set_index
 
     def set_index
-        self.index = scan.revisions.count
+        self.index ||= scan.revisions.count + 1
     end
 end
