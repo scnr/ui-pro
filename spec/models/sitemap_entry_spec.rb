caa2508dc5e8cb687b53673174d7822e617e099a
Tasos Laskos <tasos.laskos@gmail.com>
2014-09-29 10:50:00 +0300

    application.css.scss: Added ul.info

--- spec/models/sitemap_entry_spec.rb
+++ spec/models/sitemap_entry_spec.rb
@@ -3,6 +3,19 @@ require 'spec_helper'
 describe SitemapEntry do
     expect_it { to belong_to :site }
     expect_it { to belong_to :revision }
-
     expect_it { to have_many :issues }
+
+    describe :scope do
+        describe :with_issues do
+            it 'returns entries with issues'
+        end
+
+        describe :without_issues do
+            it 'returns entries without issues'
+        end
+
+        describe :default do
+            it 'sorts entries by URL'
+        end
+    end
 end
