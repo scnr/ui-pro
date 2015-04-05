0be4f5b6adccdfb6d38b447c4d48d569809d9e36
Tasos Laskos <tasos.laskos@gmail.com>
2015-04-05 15:21:45 +0300

    dispatcher.js: Disabled WebSocketRails dispatcher

--- app/assets/javascripts/dispatcher.js
+++ app/assets/javascripts/dispatcher.js
@@ -1,5 +1,5 @@
-window.dispatcher = new WebSocketRails( 'localhost:3000/websocket' );
-
-window.dispatcher.on_open = function( data ) {
-    console.log( 'Connection has been established: ', data )
-};
+//window.dispatcher = new WebSocketRails( 'localhost:3000/websocket' );
+//
+//window.dispatcher.on_open = function( data ) {
+//    console.log( 'Connection has been established: ', data )
+//};
