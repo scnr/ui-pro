69af146ba315cade54af88b0a86e885e350e8835
Tasos Laskos <tasos.laskos@gmail.com>
2015-04-05 15:31:14 +0300

    Fixed Poltergeist click issue

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,9 +1,21 @@
 require 'capybara/poltergeist'
-#
-# Capybara.register_driver :poltergeist do |app|
-#     Capybara::Poltergeist::Driver.new( app, debug: true )
-# end
-#
+
+# See: https://github.com/teampoltergeist/poltergeist/issues/520#issuecomment-84828753
+class Capybara::Node::Element
+
+    alias :old_click :click if method_defined? :click
+
+    def click
+        trigger('click')
+    rescue Capybara::NotSupportedByDriverError
+        old_click
+    end
+end
+
+Capybara.register_driver :poltergeist do |app|
+    Capybara::Poltergeist::Driver.new( app, js_errors: false )
+end
+
 # Capybara.asset_host        = 'http://localhost:3000'
 # Capybara.javascript_driver = :chrome
 
