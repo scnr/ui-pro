dbf3440cbb13e5db77d22a140da7a99c5ee9df3d
Tasos Laskos <tasos.laskos@gmail.com>
2015-08-02 13:07:08 +0300

    Removed obsolete Plan model

--- app/models/plan.rb
+++
@@ -1,7 +0,0 @@
-class Plan < ActiveRecord::Base
-    has_many :scans
-
-    has_one :profile_override, as: :profile_overridable, dependent: :destroy,
-            autosave: true
-    accepts_nested_attributes_for :profile_override
-end
