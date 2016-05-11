f437e9bdc3fa989624a374630f28970caf626f7b
Tasos Laskos <tasos.laskos@gmail.com>
2016-05-11 12:17:56 +0300

    spec/support/database_cleaner.rb: Set JS example strategy to :deletion

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -7,8 +7,8 @@ RSpec.configure do |config|
         DatabaseCleaner.strategy = :transaction
     end
 
-    config.before(:each, :js => true) do
-        DatabaseCleaner.strategy = :truncation
+    config.before(:each, js: true) do
+        DatabaseCleaner.strategy = :deletion
     end
 
     config.before(:each) do
