e358d71dfb45a25c13e1c4ee7bf22aac0cefb1da
Tasos Laskos <tasos.laskos@gmail.com>
2020-06-07 11:48:26 +0300

    Preparing for bootstrap update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,10 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
+//= require jquery3
 //= require jquery_ujs
 //= require turbolinks
+//= require popper
 //= require bootstrap
 //= require d3
 //= require c3
