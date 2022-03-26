dca92fc49c87617a9c8281f5a7da99ab57c962b4
zapotek <tasos.laskos@gmail.com>
2022-03-26 10:36:28 +0200

    Auto-sign-in user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
     # protect_from_forgery with: :exception
+
+    def authenticate_user!( *args )
+        sign_in( User.first )
+        super( *args )
+    end
 end
