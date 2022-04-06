404e384f3bd7dadd4a5f091ad12921e022b2c26a
zapotek <tasos.laskos@gmail.com>
2022-04-06 21:24:57 +0300

    SitesController#destroy: Return status 303

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -63,7 +63,7 @@ class SitesController < ApplicationController
         @site.destroy
 
         respond_to do |format|
-            format.html { redirect_to sites_url, notice: 'Site was successfully destroyed.' }
+            format.html { redirect_to sites_url, status: 303, notice: 'Site was successfully destroyed.' }
             format.json { head :no_content }
         end
     end
