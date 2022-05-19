88969f1df4b782235214eb5715f19c48c2a82a01
Tasos Laskos <tasos.laskos@gmail.com>
2022-05-19 07:34:30 +0300

    Switched to SQLite3 by default

--- config/initializers/paths.rb
+++ config/initializers/paths.rb
@@ -1,2 +1,7 @@
-FileUtils.mkdir_p ENV['SCNR_PRO_DB_DIR']
-FileUtils.mkdir_p ENV['SCNR_PRO_LOG_DIR']
+if ENV['SCNR_PRO_DB_DIR']
+    FileUtils.mkdir_p ENV['SCNR_PRO_DB_DIR']
+end
+
+if ENV['SCNR_PRO_LOG_DIR']
+    FileUtils.mkdir_p ENV['SCNR_PRO_LOG_DIR']
+end
