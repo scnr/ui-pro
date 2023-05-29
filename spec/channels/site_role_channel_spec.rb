179648aa8f342e76b16d0a426f806f2f59027583
Alexei Dmitriev <alexei.dmitriev@rubygarage.org>
2023-05-29 11:19:47 +0300

    Added channel tests

---
+++ spec/channels/site_role_channel_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+RSpec.describe SiteRoleChannel do
+  let(:current_user) { create(:user) }
+
+  before do
+    stub_connection(current_user: current_user)
+    subscribe
+  end
+
+  context 'with user' do
+    it 'subscribes to a current user stream' do
+      expect(subscription).to be_confirmed
+      expect(subscription).to have_stream_for(current_user)
+    end
+  end
+
+  context 'without user' do
+    pending('Need to be implemented')
+  end
+end
