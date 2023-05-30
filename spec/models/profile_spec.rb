c3fdc27b779b81279783a499e9dee7e8d1190311
Alexei Dmitriev <alexei.dmitriev@rubygarage.org>
2023-05-30 10:54:52 +0300

    Added profile model tests

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -1,6 +1,6 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
-describe Profile do
+RSpec.describe Profile do
     subject { FactoryGirl.create :profile, user: user }
     let(:user) { FactoryGirl.create :user }
 
@@ -13,4 +13,32 @@ describe Profile do
         end
     end
 
+    describe 'broadcast callbacks' do
+        let(:queue_name) { 'default' }
+
+        describe 'after_create_commit' do
+            subject(:profile) { build(:profile) }
+
+            # Due to spec/factories/profiles.rb#L5 callbacks are not runs.
+            xit 'calls Broadcasts::Profiles::CreateJob' do
+                expect { profile.save }.to have_enqueued_job(Broadcasts::Profiles::CreateJob).with(profile.id).on_queue(queue_name)
+            end
+        end
+
+        describe 'after_update_commit' do
+            subject(:profile) { create(:profile) }
+
+            it 'calls Broadcasts::Profiles::CreateJob' do
+                expect { profile.touch }.to have_enqueued_job(Broadcasts::Profiles::UpdateJob).with(profile.id).on_queue(queue_name)
+            end
+        end
+
+        describe 'after_destroy_commit' do
+            subject(:profile) { create(:profile, user: user) }
+
+            it 'calls Broadcasts::Profiles::CreateJob' do
+                expect { profile.destroy }.to have_enqueued_job(Broadcasts::Profiles::DestroyJob).with(profile.id, user.id).on_queue(queue_name)
+            end
+        end
+    end
 end
