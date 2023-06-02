8a44ba5feb06a640199a7876173f8a6a53c5f515
Alexei Dmitriev <alexei.dmitriev@rubygarage.org>
2023-06-02 09:54:44 +0300

    Fixed typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
-  # config.active_job.queue_adapter     = :rescue
+  # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "scnr/ui/pro_production"
 
   config.action_mailer.perform_caching = false
