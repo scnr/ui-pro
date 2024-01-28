be4a826c53f74c725318016c0770ff7ea6e9893a
Tasos Laskos <tasos.laskos@gmail.com>
2024-01-28 11:01:04 +0200

    Added license guard

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,13 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 require 'scnr/application'
 
+begin
+    SCNR::License.guard! :dev, :trial, :pro, :enterprise
+rescue SCNR::License::Error => e
+    puts "[ERROR] #{e}"
+    exit 1
+end
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
